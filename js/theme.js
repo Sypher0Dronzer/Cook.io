bfb77facf92c608ecc1d3ad8dcbb93570fda843d
Soham Saha <83546205+Sypher0Dronzer@users.noreply.github.com>
2024-04-14 10:43:57 +0530

    starting

---
+++ js/theme.js
@@ -0,0 +1,7 @@
+// document.body.classList.add('darktheme')
+
+const themeSwitch=document.querySelector('.right-nav toggle')
+themeSwitch.addEventListener('click',()=>{
+document.body.classList.toggle('darktheme')
+
+})
